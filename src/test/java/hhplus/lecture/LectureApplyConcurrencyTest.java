5e6e5754876026a3d8acc50d3d2ce29928563af2
JunGyeongCho <scope0204@gmail.com>
2024-10-04 05:10:58 +0900

    test: 동일한 유저 정보로 같은 특강을 5번 신청했을 때, 1번만 성공하는 것을 검증하는 통합 테스트 작성

--- src/test/java/hhplus/lecture/LectureApplyConcurrencyTest.java
+++ src/test/java/hhplus/lecture/LectureApplyConcurrencyTest.java
@@ -90,4 +90,51 @@ public class LectureApplyConcurrencyTest {
         assertThat(histories.stream().filter(history -> history.isApplyStatus() == true).count()).isEqualTo(30);
         assertThat(histories.stream().filter(history -> history.isApplyStatus() == false).count()).isEqualTo(10);
     }
+
+    @Test
+    @DisplayName("동일한 회원이 동시에 강의 5번 신청 시, 1번만 강의 신청 되는 것을 검증한다")
+    void shouldAllowSingleEnrollmentWhenStudentApplies5TimesSimultaneously() throws InterruptedException {
+        Long userId = 1L;
+        Long lectureId = 1L;
+
+        int threadCount = 5;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 1; i <= threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    LectureApplyDto lectureApplyDto = new LectureApplyDto(userId, lectureId);
+                    lectureApplyFacade.lectureApply(lectureApplyDto);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        // applyHistoryRepository에서 특정 userId와 lectureId로 저장된 신청 기록을 조회
+        List<ApplyHistory> histories = applyHistoryRepository.findByUserIdAndLectureId(userId, lectureId);
+
+        // 신청 기록이 1건만 성공했는지 확인
+        long successCount = histories.stream()
+                .filter(history -> history.isApplyStatus() == true)
+                .count();
+
+        long failCount = histories.stream()
+                .filter(history -> history.isApplyStatus() == false)
+                .count();
+
+        // 성공한 신청이 1건이어야 함
+        assertThat(successCount).isEqualTo(1);
+
+        // 실패한 신청이 4건이어야 함
+        assertThat(failCount).isEqualTo(4);
+    }
+
 }
